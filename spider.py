0abd08c613fc77c731d5d10d53d34dc0cf0717a3
8201cai <42993650+8201cai@users.noreply.github.com>
2018-09-14 10:18:39 +0800

    猫眼电影排行
    100

--- spider.py
+++ spider.py
@@ -1,79 +1,53 @@
+import json
 import requests
-from urllib.parse import urlencode
-from requests import codes
-import os
-from hashlib import md5
-from multiprocessing.pool import Pool
+from requests.exceptions import RequestException
+import re
+import time
 
 
-def get_page(offset):
-    params = {
-        'offset': offset,
-        'format': 'json',
-        'keyword': '街拍',
-        'autoload': 'true',
-        'count': '20',
-        'cur_tab': '1',
-        'from': 'search_tab'
-    }
-    base_url = 'https://www.toutiao.com/search_content/?'
-    url = base_url + urlencode(params)
+def get_one_page(url):
     try:
-        resp = requests.get(url)
-        if codes.ok == resp.status_code:
-            return resp.json()
-    except requests.ConnectionError:
+        headers = {
+            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.162 Safari/537.36'
+        }
+        response = requests.get(url, headers=headers)
+        if response.status_code == 200:
+            return response.text
+        return None
+    except RequestException:
         return None
 
 
-def get_images(json):
-    if json.get('data'):
-        data = json.get('data')
-        for item in data:
-            if item.get('cell_type') is not None:
-                continue
-            title = item.get('title')
-            images = item.get('image_list')
-            for image in images:
-                yield {
-                    'image': 'https:' + image.get('url'),
-                    'title': title
-                }
+def parse_one_page(html):
+    pattern = re.compile('<dd>.*?board-index.*?>(\d+)</i>.*?data-src="(.*?)".*?name"><a'
+                         + '.*?>(.*?)</a>.*?star">(.*?)</p>.*?releasetime">(.*?)</p>'
+                         + '.*?integer">(.*?)</i>.*?fraction">(.*?)</i>.*?</dd>', re.S)
+    items = re.findall(pattern, html)
+    for item in items:
+        yield {
+            'index': item[0],
+            'image': item[1],
+            'title': item[2],
+            'actor': item[3].strip()[3:],
+            'time': item[4].strip()[5:],
+            'score': item[5] + item[6]
+        }
 
 
-def save_image(item):
-    img_path = 'img' + os.path.sep + item.get('title')
-    if not os.path.exists(img_path):
-        os.makedirs(img_path)
-    try:
-        resp = requests.get(item.get('image'))
-        if codes.ok == resp.status_code:
-            file_path = img_path + os.path.sep + '{file_name}.{file_suffix}'.format(
-                file_name=md5(resp.content).hexdigest(),
-                file_suffix='jpg')
-            if not os.path.exists(file_path):
-                with open(file_path, 'wb') as f:
-                    f.write(resp.content)
-                print('Downloaded image path is %s' % file_path)
-            else:
-                print('Already Downloaded', file_path)
-    except requests.ConnectionError:
-        print('Failed to Save Image，item %s' % item)
+def write_to_file(content):
+    with open('result.txt', 'a', encoding='utf-8') as f:
+        f.write(json.dumps(content, ensure_ascii=False) + '\n')
 
 
 def main(offset):
-    json = get_page(offset)
-    for item in get_images(json):
+    url = 'http://maoyan.com/board/4?offset=' + str(offset)
+    html = get_one_page(url)
+    for item in parse_one_page(html):
         print(item)
-        save_image(item)
+        write_to_file(item)
 
-
-GROUP_START = 0
-GROUP_END = 7
 
 if __name__ == '__main__':
-    pool = Pool()
-    groups = ([x * 20 for x in range(GROUP_START, GROUP_END + 1)])
-    pool.map(main, groups)
-    pool.close()
-    pool.join()
+    for i in range(10):
+        main(offset=i * 10)
+        time.sleep(1)
